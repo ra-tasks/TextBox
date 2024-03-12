2e2d38af5882f0f39d8435a2384584c722677548
Ben Bogin <benb@allenai.org>
2024-03-12 10:12:11 -0700

    easier installation

--- install.sh
+++ install.sh
@@ -21,7 +21,8 @@ pip install -r requirements.txt
 
 echo -e "\033[0;32mInstalling requirements (rouge) ...\033[0m"
 pip install -U git+https://github.com/pltrdy/pyrouge.git
-git clone https://github.com/pltrdy/files2rouge.git 
+git clone https://github.com/pltrdy/files2rouge.git
+
 cd files2rouge
 python setup_rouge.py
 python setup.py install
